80f58e771c685c055b183331fe2caf2d974a2989
apm-code <adrianignacio.perez832@comunidadunir.net>
2025-02-09 01:46:35 +0100

    Add: esquema del menú

--- src/testing/Main.java
+++ src/testing/Main.java
@@ -2,6 +2,15 @@ package testing;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+        System.out.println("- - - - - - - - - -");
+        System.out.println("- - CALCULADORA - -");
+        System.out.println("- - - - - - - - - -");
+        System.out.println("Selecciona una opción:");
+        System.out.println("1. Suma");
+        System.out.println("2. Resta");
+        System.out.println("3. Producto");
+        System.out.println("4. Cociente");
+
     }
 }
