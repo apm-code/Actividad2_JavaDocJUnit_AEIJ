0907f1fdd105cc564c381ade012a3fbf0f608709
Javier Garcia Ruiz <javier.garcia433@comunidadunir.net>
2025-02-10 18:45:46 +0100

    Creacion rama producto

--- src/javabeans/Producto_Javier.java
+++ src/javabeans/Producto_Javier.java
@@ -1,4 +1,24 @@
 package javabeans;
 
+
+/**
+ * Clase que implementa métodos relacionados con las multiplicaiones. Tiene los
+ * siguientes métodos.
+ * 1. Producto de dos números reales.
+ * 2. Producto de dos números enteros.
+ * 3. Producto de tres números reales.
+ * 4. Potencia.
+ *
+ * @author Javier García Ruiz
+ * @version 1.0
+ */
+
 public class Producto_Javier {
+
+    private double numReal1;
+    private double numReal2;
+
+    private int numEntero1;
+    private int numEntero2;
+
 }
