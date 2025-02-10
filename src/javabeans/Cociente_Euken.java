1e206194859be981e1b5e442df79fb9620dfc306
eukensaez <euken.saez115@comunidadunir.net>
2025-02-10 13:52:12 +0100

    Modificada la clase Cociente.

--- src/javabeans/Cociente_Euken.java
+++ src/javabeans/Cociente_Euken.java
@@ -1,6 +1,19 @@
 package javabeans;
 
-import java.util.Objects;
+import java.util.Scanner;
+
+/**
+ * Clase que implementa métodos relacionados con las divisiones. Tiene los
+ * siguientes métodos.
+ * 1. División de dos números reales.
+ * 2. División de dos números enteros.
+ * 3. Inversa de dos números reales.
+ * 4. Raiz cuadrada de un número entero.
+ *
+ * @author eukensaez
+ * @version 1.0
+ */
+
 
 public class Cociente_Euken {
 
@@ -10,104 +23,78 @@ public class Cociente_Euken {
     private int numEntero1;
     private int numEntero2;
 
-    //Constructor
+    private int opcionMenu;
 
-    public Cociente_Euken() {
-        this.numReal1 = numReal1;
-        this.numReal2 = numReal2;
+
+    public void menuDivision(){
+        do {
+            System.out.println("Menu cocientes. Selecciona una opcion para el calculo");
+            System.out.println("1. Division de dos numeros reales");
+            System.out.println("2. Division de dos numeros enteros");
+            System.out.println("3. Inversa de dos numeros reales");
+            System.out.println("4. Raiz cuadrada de dos numeros enteros");
+
+            Scanner leer = new Scanner(System.in);
+            opcionMenu = leer.nextInt();
+
+            switch (opcionMenu) {
+                case 1:
+                    System.out.println("Dame un número real");
+                    double numReal1 = leer.nextDouble();
+                    System.out.println("Dame otro número real");
+                    double numReal2 = leer.nextDouble();
+
+                    if (numReal2 != 0) {
+                        double resultado = numReal1 / numReal2;
+                        System.out.println("El resultado de la division real es " + resultado);
+                    } else {
+                        System.out.println("El cálculo de la division real es nulo.");
+                    }
+                    break;
+
+                case 2:
+                    System.out.println("Dame un número entero");
+                    int numEntero1 = leer.nextInt();
+                    System.out.println("Dame otro número entero");
+                    int numEntero2 = leer.nextInt();
+
+                    if (numEntero2 != 0) {
+                        double resultado = numEntero1 / numEntero2;
+                        System.out.println("El resultado de la division entera es " + resultado);
+                    } else {
+                        System.out.println("El cálculo de la division entera es nulo");
+                    }
+
+                case 3:
+                    System.out.println("Dame un número real");
+                    numReal2 = leer.nextDouble();
+
+                    if (numReal2 != 0) {
+                        double resultado = 1 / numReal2;
+                        System.out.println("El resultado de la inversa real es " + resultado);
+                    } else {
+                        System.out.println("El resultado de la inversa real es nulo");
+                    }
+                    break;
+
+                case 4:
+                    System.out.println("Dame un número real");
+                    numReal1 = leer.nextDouble();
+
+                    if (numReal1 >= 0) {
+                        double resultado = Math.sqrt(numReal1);
+                        System.out.println("El resultado del raiz cuadrada es " + resultado);
+                    } else {
+                        System.out.println("El resultado del raiz cuadrada es nulo");
+                    }
+                    break;
+
+                default:
+                    System.out.println("Opcion no valida");
+
+            }
+        } while (opcionMenu>=1 || opcionMenu <=4);
+
+        System.out.println("Fin del programa");
     }
-
-
-    // Getter and setter
-
-    public double getnumReal1() {
-        return numReal1;
-    }
-
-    public double getnumReal2() {
-        return numReal2;
-    }
-
-    public void setnumReal1(double numReal1) {
-        this.numReal1 = numReal1;
-    }
-
-    public void setnumReal2(double numReal2) {
-        this.numReal2 = numReal2;
-    }
-
-
-    //Equals y hascode
-
-    @Override
-    public boolean equals(Object o) {
-        if (o == null || getClass() != o.getClass()) return false;
-        Cociente_Euken that = (Cociente_Euken) o;
-        return Double.compare(numReal1, that.numReal1) == 0 && Double.compare(numReal2, that.numReal2) == 0;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(numReal1, numReal2);
-    }
-
-
-    //toString
-
-    @Override
-    public String toString() {
-        return "Cociente_Euken{" + "numReal1=" + numReal1 + ", numReal2=" + numReal2 + '}';
-    }
-
-
-    //Métodos propios
-
-    //Método1
-
-    public void divisionReales(double numReal1, double numReal2){
-        if (numReal2 != 0) {
-            double resultado = numReal1 / numReal2;
-            System.out.println("El resultado de la division real es " + resultado);
-        }
-        else {
-            System.out.println("El cálculo de la division real es nulo.");
-        }
-    }   
-
-    //Método2
-
-    public void divisionEnteros(int numEntero1, int numEntero2){
-        if (numEntero2 != 0) {
-            int resultado = numEntero1 / numEntero2;
-            System.out.println("El resultado de la division entera es " + resultado);
-        }
-        else {
-            System.out.println("El cálculo de la division entera es nulo");
-        }
-    }
-
-    //Método3
-
-    public void inversoReales(double numReal2){
-        if (numReal2 != 0) {
-            double resultado = 1 / numReal2;
-            System.out.println("El resultado del inverso real es " + resultado);
-        }
-        else {
-            System.out.println("El resultado del inverso es nulo");
-        }
-    }
-
-    //Método4
-
-    public void raizNumero(double numReal1){
-        if (numReal1 >= 0) {
-            double resultado = Math.sqrt(numReal1);
-            System.out.println("El resultado del raiz cuadrada es " + resultado);
-        }
-        else {
-            System.out.println("El resultado del raiz cuadrada es nulo");
-        }
-    }
-
 }
--- src/testing/Main.java
+++ src/testing/Main.java
@@ -1,24 +1,5 @@
 package testing;
 
-import javabeans.Cociente_Euken;
-
-import java.util.Scanner;
-
-public class Main {
-    public static void main(String[] args) {
-
-
-    /* Cociente_Euken divreal1 = new Cociente_Euken();
-        Scanner leer = new Scanner(System.in);
-
-        System.out.println("Dame un número real");
-        double numReal1 = leer.nextDouble();
-        System.out.println("Dame otro número real");
-        double numReal2 = leer.nextDouble();
-
-        divreal1.divisionReales(numReal1, numReal2);
-      */
-    }
 
 
 
@@ -26,5 +7,3 @@ public class Main {
 
 
 
-
-}
