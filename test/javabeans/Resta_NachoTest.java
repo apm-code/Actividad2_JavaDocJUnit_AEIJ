2f4b78d94e73c1434e86d5b157e8a39c0ed24062
apm-code <adrianignacio.perez832@comunidadunir.net>
2025-02-15 11:21:24 +0100

    Add: test producto y resta

---
+++ test/javabeans/Resta_NachoTest.java
@@ -0,0 +1,33 @@
+package javabeans;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Resta_NachoTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void restaDosReales() {
+    }
+
+    @Test
+    void restaDosEnteros() {
+    }
+
+    @Test
+    void restaTresReales() {
+    }
+
+    @Test
+    void restaAcumulada() {
+    }
+
+    @Test
+    void getValorAcumulado() {
+    }
+}
