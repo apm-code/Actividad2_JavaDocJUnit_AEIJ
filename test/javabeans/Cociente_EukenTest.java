8c38cbd321a4ff0ea9526b54bb7b982795d74d10
apm-code <adrianignacio.perez832@comunidadunir.net>
2025-02-15 12:51:28 +0100

    Add: test de la clase cociente

---
+++ test/javabeans/Cociente_EukenTest.java
@@ -0,0 +1,24 @@
+package javabeans;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Cociente_EukenTest {
+
+    @Test
+    void divisionDosReales() {
+    }
+
+    @Test
+    void divisionDosEnteros() {
+    }
+
+    @Test
+    void inversaReal() {
+    }
+
+    @Test
+    void raizCuadrada() {
+    }
+}
