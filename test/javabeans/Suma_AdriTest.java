8d0f56856816e365af2e5f8ffec66504c7ac75f7
apm-code <adrianignacio.perez832@comunidadunir.net>
2025-02-15 10:40:15 +0100

    Add: test de prueba

---
+++ test/javabeans/Suma_AdriTest.java
@@ -0,0 +1,26 @@
+package javabeans;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Suma_AdriTest {
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void sumaDosReales() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void sumaDosEnteros() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void sumaTresReales() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void sumaAcumulada() {
+    }
+}
